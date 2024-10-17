541840bbfa4d0c452c0f1d255306158b81c34df1
Soly <82676469+Solyphonous@users.noreply.github.com>
2024-10-17 20:24:25 +0100

    Update redirector.js

--- redirector.js
+++ redirector.js
@@ -1,2 +1,16 @@
-var url = window.location.host
-console.log(url)
+// Funcs
+
+function getSubdomain(host) {
+    const regex = /^([^\.]+)\./
+    const match = host.match(regex)
+    return match ? match[1] : null;
+}
+
+// Main
+
+const url = window.location.host
+const subdomain = getSubdomain(url)
+
+if (subdomain == "p") {
+    window.location.replace("https://rentry.co/phy")
+}
