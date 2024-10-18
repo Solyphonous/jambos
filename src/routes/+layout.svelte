5d85e3e42b5b1e792b4e3a343d90a06aa8e46b95
Soly <82676469+Solyphonous@users.noreply.github.com>
2024-10-18 20:29:47 +0100

    add title and jambo judah

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,7 +2,31 @@
     import '../app.css';
     import Redirector from "../lib/redirector.svelte";
     import Header from "../lib/header.svelte";
+
+    import { browser } from "$app/environment"
+    import { afterNavigate } from "$app/navigation"
+
+    let currentpath = browser ? window.location.pathname : "/"
+
+    if (browser) {
+        afterNavigate(() => {
+            currentpath = window.location.pathname;
+        });
+    }
+
+    $: pagetitle = currentpath.substring(1).charAt(0).toUpperCase() + currentpath.substring(1).slice(1)
+
 </script>
+
+<head>
+    {#if (pagetitle != "")}
+        <title>The Jambos Casa - {pagetitle}</title>
+    {:else}
+        <title>The Jambos Casa - Home</title>
+    {/if}
+
+    <meta content="images/jambojudah.png" property="og:image" />
+</head>
 
 <Redirector />
 <Header />
