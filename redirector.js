20467ea0fdb92bf1b223282dcb1f7c541435da65
Soly <82676469+Solyphonous@users.noreply.github.com>
2024-10-18 09:07:26 +0100

    studied the ways of switchcase-jitsu and returned enlightened with deep knowledge

--- redirector.js
+++ redirector.js
@@ -17,12 +17,12 @@ const subdomain = getSubdomain(url)
 
 switch(subdomain) {
     case "p":
-        console.log(url, subdomain)
-        //loadPage("https://rentry.co/phy")
+        loadPage("https://rentry.co/phy")
+        break
     case "soly":1
-        console.log(url, subdomain)
-        //loadPage("https://solyphonous.carrd.co/")
+        loadPage("https://solyphonous.carrd.co/")
+        break
     case "seb":
-        console.log(url, subdomain)
-        //loadPage("https://sebistan.carrd.co/")
+        loadPage("https://sebistan.carrd.co/")
+        break
 }
