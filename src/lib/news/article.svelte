349bcd294c610a4018fcff44c2ba509c16f60662
Soly <82676469+Solyphonous@users.noreply.github.com>
2024-10-20 00:03:48 +0100

    News (#1)
    * change jambo judah link just for fun
    
    * add news
    
    * add first article

---
+++ src/lib/news/article.svelte
@@ -0,0 +1,10 @@
+<script>
+    export let article
+    let articlename = article.substring(0, article.indexOf("."))
+</script>
+
+<p>
+    <a href="news/{articlename}">
+        {articlename}
+    </a>
+</p>
---
+++ src/routes/news/+page.svelte
@@ -0,0 +1,8 @@
+<script>
+    export let data
+    import Article from '../../lib/news/article.svelte';
+</script>
+
+{#each data.articles.reverse() as article}
+    <Article article={article}/>
+{/each}
