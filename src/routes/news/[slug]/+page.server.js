eac3f12de6eea7177eab146c8328bcf3f5c3b305
Soly <82676469+Solyphonous@users.noreply.github.com>
2024-10-20 16:10:22 +0100

    Revert "Merge branch 'main' into news"
    This reverts commit 79bcb84b5cc57a7aed7d637e966cfe550ee629f3, reversing
    changes made to ba6e1b1fc413d3fbc0d7a13ecbbbd1c2998f3107.

--- src/routes/news/[slug]/+page.server.js
+++
@@ -1,9 +0,0 @@
-import { marked } from "marked"
-import fs from "fs"
-
-export function load ({ params }) {
-    return { 
-        title: params.slug,
-        content: marked.parse(fs.readFileSync("content/news/"+params.slug+".md", "utf-8"))
-     }
-}
--- src/routes/news/[slug]/+page.svelte
+++
@@ -1,5 +0,0 @@
-<script>
-    export let data
-</script>
-
-<div>{@html data.content }</div>
