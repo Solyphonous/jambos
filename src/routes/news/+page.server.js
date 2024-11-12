46f224fa31bf79badaebdeecc238eb276e796865
Soly <82676469+Solyphonous@users.noreply.github.com>
2024-11-12 13:59:20 +0000

    maybe env fix five million

--- src/routes/news/+page.server.js
+++ src/routes/news/+page.server.js
@@ -1,5 +1,7 @@
+import { env } from "$env/static/private";
+
 async function getNews() {
-    const WorkersAPIKey = process.env.WorkersAPIKey
+    const WorkersAPIKey = env.WorkersAPIKey
     const response = await fetch("https://jambos-worker.jopogb.workers.dev/api/list", {
         headers: {
             "Authorization": WorkersAPIKey
