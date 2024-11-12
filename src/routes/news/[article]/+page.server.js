f1be9832cd581edbd14bff6e1371ec4c888e8806
Soly <82676469+Solyphonous@users.noreply.github.com>
2024-11-12 11:06:23 +0000

    change articles to use html instead of markdown because it sucked a lot

--- src/routes/news/[article]/+page.server.js
+++ src/routes/news/[article]/+page.server.js
@@ -1,5 +1,3 @@
-import { marked } from "marked"
-
 export async function load ({ params }) {
     const title = params.article
     const encodedTitle = title.replace(/ /g, "-")
@@ -9,14 +7,13 @@ export async function load ({ params }) {
 
     const response = await fetch(`https://jambos-worker.jopogb.workers.dev/api/article/${encodedTitle}`)
     const data = await response.json()
-    console.log(data)
     if (response.ok) {
-        content = marked.parse(data.content)
+        content = data.content
         author = data.author
     } else {
         content = `<h1>Error loading article: ${data.error}</h1>`
     }
-    
+
     return { 
         title: title,
         author: author,
