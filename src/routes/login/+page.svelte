eed3d323063ebd512fa30361949656570b5d3d09
Soly <82676469+Solyphonous@users.noreply.github.com>
2024-12-18 15:27:39 +0000

    style header, login and create acount pages

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,15 +1,5 @@
 <script>
-    // Login
-    import Userform from "../../lib/userform.svelte";
+	import Userdetailspage from "../../lib/userdetailspage.svelte";
 </script>
 
-<h1>Login</h1>
-<Userform actionName="Log in"/>
-
-<p>Fool, do you have not an account?! Worry not. <a href="signup">Create it</a></p>
-
-<style>
-    a {
-        font-weight: bold;
-    }
-</style>
+<Userdetailspage pageType={"login"}/>
