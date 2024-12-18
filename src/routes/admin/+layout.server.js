2aabf2c7a8495fba029f62b0e7f166c024ac5428
Soly <82676469+Solyphonous@users.noreply.github.com>
2024-12-18 09:58:49 +0000

    add protected admin page

---
+++ src/routes/admin/+layout.server.js
@@ -0,0 +1,27 @@
+import { redirect } from '@sveltejs/kit'
+import jwt from "jsonwebtoken"
+import { env } from "$env/dynamic/private";
+
+export function load({ cookies }) {
+    const token = cookies.get("login")
+
+    if (!token) {
+        redirect(302, "/login?redirect=admin")
+    }
+
+    var logintoken
+
+    try {
+        logintoken = jwt.verify(token, env.JWTSigningKey, { algorithm: "HS256" })
+    } catch {
+        console.warn("Cookie invalid")
+        redirect(302, "/")
+    }
+
+    if (logintoken.rank < 255) {
+        console.warn("Rank too low")
+        redirect(302, "/")
+    }
+
+    return { token: logintoken }
+}
