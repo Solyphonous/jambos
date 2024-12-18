ad50c2f421337c2ff297988992d897e40a128cd1
Soly <82676469+Solyphonous@users.noreply.github.com>
2024-12-18 14:17:55 +0000

    style login pages verygood verynice

--- src/routes/+layout.server.js
+++ src/routes/+layout.server.js
@@ -1,6 +1,14 @@
 // Root server
 
-export async function load({ locals }) {
-    let token = locals.token
-    return { token: token }
+import jwt from "jsonwebtoken"
+import { env } from "$env/dynamic/private";
+
+export async function load({ cookies }) {
+    let token = cookies.get("login")
+    try {
+        const logintoken = jwt.verify(token, env.JWTSigningKey, { algorithm: "HS256" })
+        return { token: logintoken }
+    } catch {
+        return { token: null }
+    }
 }
