57df938b9f68244f5da2a763f11dcb853960096c
Soly <82676469+Solyphonous@users.noreply.github.com>
2024-12-18 09:41:00 +0000

    fully implement logout

--- src/routes/+layout.server.js
+++ src/routes/+layout.server.js
@@ -1,12 +1,6 @@
 // Root server
 
-export async function load({ cookies }) {
-    const token = cookies.get("login")
-    
-    if (token) {
-        return { token: token }
-    } else {
-        console.log("Root server: No token could be loaded from cookies")
-        return { token: null}
-    }
+export async function load({ locals }) {
+    let token = locals.token
+    return { token: token }
 }
