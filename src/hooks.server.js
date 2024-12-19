fc2b481182979dee7b34345de9d1dd7688905372
Soly <82676469+Solyphonous@users.noreply.github.com>
2024-12-19 09:05:37 +0000

    implement token caching to prevent excessive backend requests

--- src/hooks.server.js
+++ src/hooks.server.js
@@ -1,22 +1,33 @@
 import { verifyJWT } from './lib/verifyJWT'
 
+var cachedUnverifiedToken
+var cachedToken
+
 export async function handle({ event, resolve }) {
     const unverifiedToken = event.cookies.get("token")
 
+    // Check if user is logged in
     if (!unverifiedToken) {
         event.locals.token = null
         return await resolve(event)
     }
 
-    if (!event.locals.token) {
-        const token = await verifyJWT(unverifiedToken)
+    // Check if user token is the same as last request, if so return cached cookie that wouldve been decoded on first request
+    if (unverifiedToken == cachedUnverifiedToken) {
+        event.locals.token = cachedToken
+        return await resolve(event)
+    }
 
-        if (token) {
-            console.log(`Got decoded token: ${JSON.stringify(token)}`)
-            event.locals.token = token
-        } else {
-            event.locals.token = null
-        }
+    // Try to decode token on backend
+    const token = await verifyJWT(unverifiedToken)
+
+    // If we get a token we cache it and put it in locals for load functions to use
+    if (token) {
+        cachedUnverifiedToken = unverifiedToken
+        cachedToken = token
+        event.locals.token = token
+    } else {
+        event.locals.token = null
     }
 
     return await resolve(event)
