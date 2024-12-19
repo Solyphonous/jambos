b216be099963124cdec9a18bd7e5a5c708e39976
Soly <82676469+Solyphonous@users.noreply.github.com>
2024-12-19 11:08:17 +0000

    remove caching because it was actually incredibly poorly implemented and sucked huge dick

--- src/hooks.server.js
+++ src/hooks.server.js
@@ -1,10 +1,5 @@
 import { verifyJWT } from './lib/verifyJWT'
 
-// THIS IS AWFUL AND BAD AND SHARES TOKENS WITH OTHER USERS! FIX LATER
-
-var cachedUnverifiedToken
-var cachedToken
-
 export async function handle({ event, resolve }) {
     const unverifiedToken = event.cookies.get("token")
 
@@ -14,19 +9,10 @@ export async function handle({ event, resolve }) {
         return await resolve(event)
     }
 
-    // Check if user token is the same as last request, if so return cached cookie that wouldve been decoded on first request
-    if (unverifiedToken == cachedUnverifiedToken) {
-        event.locals.token = cachedToken
-        return await resolve(event)
-    }
-
     // Try to decode token on backend
     const token = await verifyJWT(unverifiedToken)
 
-    // If we get a token we cache it and put it in locals for load functions to use
     if (token) {
-        cachedUnverifiedToken = unverifiedToken
-        cachedToken = token
         event.locals.token = token
     } else {
         event.locals.token = null
