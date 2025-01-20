12d37299203da32d3ccdd524e999b4d9433823c8
Soly <82676469+Solyphonous@users.noreply.github.com>
2025-01-20 12:16:16 +0000

    frontend news editor

---
+++ src/routes/admin/news/+page.svelte
@@ -0,0 +1,28 @@
+<script>
+    export let data
+    $: title = "Article Title"
+    $: content = `<p>Enter your article here...</p>`
+</script>
+
+<form method="POST">
+    <label for="title">
+        Title
+        <input type="text" name="title" bind:value={title} required>
+    </label>
+    <br>
+    <label for="image">
+        Thumbnail resource
+        <input type="text" name="image" placeholder="/images/nathan.webp" required>
+    </label>
+    <br>
+
+    <textarea bind:value={content} name="" id="" rows=20 cols=100></textarea>
+    <br>
+    <button>Submit</button>
+</form>
+
+<div id="preview">
+    <h1>{title}</h1>
+    <h2>by {data.token.username}</h2>
+    {@html content}
+</div>
