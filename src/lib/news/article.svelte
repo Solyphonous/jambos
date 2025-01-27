26c2b2360e1de837850179cda2b21f6054f04c9f
Soly <82676469+Solyphonous@users.noreply.github.com>
2025-01-27 12:33:01 +0000

    implement nicer looking news page

--- src/lib/news/article.svelte
+++ src/lib/news/article.svelte
@@ -2,8 +2,33 @@
     export let article
 </script>
 
-<p>
-    <a href="news/{article}">
-        {article}
+<div class="article">
+    <a href="news/{article.title}">
+        <div class="image-container">
+            <img src="{article.thumbnail ? article.thumbnail : "/images/nathan.webp"}" alt="">
+        </div>
+        <p>{article.title}</p>
+        <p>by <b>{article.author}</b></p>
     </a>
-</p>
+</div>
+
+<style>
+    .article {
+        border-style: dashed;
+        border-width: .2rem;
+        border-color: white;
+        margin: 1rem;
+    }
+    .image-container {
+        height: 10rem;
+        width: 20rem;
+        overflow: hidden;
+        padding: .5rem;
+    }
+
+    img {
+        max-width: 100%;
+        max-height: 100%;
+        object-fit: contain;
+    }
+</style>
--- src/routes/news/+page.server.js
+++ src/routes/news/+page.server.js
@@ -1,4 +1,5 @@
 import { env } from "$env/dynamic/private";
+import { fetchArticle } from "../../lib/fetchArticle";
 
 async function getNews() {
     const response = await fetch(`${env.WorkerURL}/list`, {
@@ -19,7 +20,12 @@ async function getNews() {
 export async function load () {
     let news = await getNews()
 
+    var articles = []
+    for (var i = 0; i < news.length; i++) {
+        articles.push(await fetchArticle(news[i].name))
+    }
+
     return {
-        articles: news
+        articles: articles
     }
 }
