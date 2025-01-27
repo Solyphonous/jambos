fac386ae3a23399b13aa4fd08d97d348f09dcbe7
Soly <82676469+Solyphonous@users.noreply.github.com>
2025-01-27 12:32:01 +0000

    Move fetch article to its own function

--- src/routes/news/[article]/+page.server.js
+++ src/routes/news/[article]/+page.server.js
@@ -1,31 +1,8 @@
 // Article server
 
-import { env } from "$env/dynamic/private";
+import { fetchArticle } from "../../../lib/fetchArticle.js";
 
 export async function load ({ params }) {
-
-    const title = params.article
-    const encodedTitle = title.replace(/ /g, "-")
-
-    var content = `<h1>No article content</h1>`
-    var author = `Nobody`
-
-    const response = await fetch(`${env.WorkerURL}/article/${encodedTitle}`, {
-        headers: {
-            "Authorization": env.WorkersAPIKey
-        }
-    })
-    const data = await response.json()
-    if (response.ok) {
-        content = data.content
-        author = data.author
-    } else {
-        content = `<h1>Error loading article: ${data.error}</h1>`
-    }
-
-    return { 
-        title: title,
-        author: author,
-        content: content
-    }
+    var article = fetchArticle(params.article)
+    return article
 }
