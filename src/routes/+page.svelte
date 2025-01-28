969234ebc5409404e3db94049d665853fe293b02
Soly <82676469+Solyphonous@users.noreply.github.com>
2025-01-28 10:03:13 +0000

    add evil page biutton to homeeee

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,3 +4,12 @@
 <p style="text-align: center;"> Merry cash day from the Jambos team here in Earth </p>
     
 <img src="/images/nathanmoney.jpg" alt="hi" style="margin:auto;display:block">
+<button><a href="/linganguli">press here for evil page</a></button>
+
+<style>
+    button {
+        background-color: red;
+        margin: auto;
+        display: block;
+    }
+</style>
