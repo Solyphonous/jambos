17921931f5748cbd8bd1540b3b86587e0217cbcf
Solyphonous <82676469+Solyphonous@users.noreply.github.com>
2025-02-03 20:48:29 +0000

    Add preview for article component in article creator

--- src/routes/news/+page.server.js
+++ src/routes/news/+page.server.js
@@ -1,31 +1,29 @@
-import { env } from "$env/dynamic/private";
-import { fetchArticle } from "../../lib/fetchArticle";
+import { env } from '$env/dynamic/private';
+import { fetchArticle } from '../../lib/fetchArticle';
 
 async function getNews() {
-    const response = await fetch(`${env.WorkerURL}/list`, {
-        headers: {
-            "Authorization": env.WorkersAPIKey
-        }
-    })
-    const data = await response.json()
-    if (response.ok) {
-        return data
-    } else {
-        return [
-            {"name": `Error ${response.status} fetching articles: ${data.error}`}
-        ]
-    }
+	const response = await fetch(`${env.WorkerURL}/list`, {
+		headers: {
+			Authorization: env.WorkersAPIKey
+		}
+	});
+	const data = await response.json();
+	if (response.ok) {
+		return data;
+	} else {
+		return [{ name: `Error ${response.status} fetching articles: ${data.error}` }];
+	}
 }
 
-export async function load () {
-    let news = await getNews()
+export async function load() {
+	let news = await getNews();
 
-    var articles = []
-    for (var i = 0; i < news.length; i++) {
-        articles.push(await fetchArticle(news[i].name))
-    }
+	var articles = [];
+	for (var i = 0; i < news.length; i++) {
+		articles.push(await fetchArticle(news[i].name));
+	}
 
-    return {
-        articles: articles
-    }
+	return {
+		articles: articles
+	};
 }
--- src/routes/news/+page.svelte
+++ src/routes/news/+page.svelte
@@ -1,19 +1,10 @@
 <script>
-    export let data
-    import Article from '../../lib/news/article.svelte';
+	export let data;
+	import Article from '../../lib/news/article.svelte';
 </script>
 
 <div class="article-grid">
-    {#each data.articles as article}
-        <Article article={article}/>
-    {/each}
+	{#each data.articles as article}
+		<Article {article} />
+	{/each}
 </div>
-
-<style>
-    .article-grid {
-        display: flex;
-        justify-content: center;
-        width: auto;
-        flex-wrap: wrap;
-    }
-</style>
