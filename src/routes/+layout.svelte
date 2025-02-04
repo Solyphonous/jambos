fede8641ba361554028a9c46917875a2655ff879
Solyphonous <82676469+Solyphonous@users.noreply.github.com>
2025-02-04 17:17:14 +0000

    Remove unneccessary metadata

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,46 +1,43 @@
 <script>
-    export let data;
+	export let data;
 
-    import '../app.css';
-    import Redirector from "../lib/redirector.svelte";
-    import Header from "../lib/header.svelte";
+	import '../app.css';
+	import Redirector from '../lib/redirector.svelte';
+	import Header from '../lib/header.svelte';
 
-    import { browser } from "$app/environment"
-    import { afterNavigate } from "$app/navigation"
+	import { browser } from '$app/environment';
+	import { afterNavigate } from '$app/navigation';
 
-    let currentpath = browser ? window.location.pathname : "/"
+	let currentpath = browser ? window.location.pathname : '/';
 
-    if (browser) {
-        afterNavigate(() => {
-            currentpath = window.location.pathname;
-        });
-    }
+	if (browser) {
+		afterNavigate(() => {
+			currentpath = window.location.pathname;
+		});
+	}
 
-    $: pagetitle = currentpath.slice(1).charAt(0).toUpperCase() + currentpath.slice(2)
-    
-    $: {
-        if (pagetitle == "") {
-            pagetitle = "Home"
-        }
-    }
-    
-    $: isArticle = /News\/.+/.test(pagetitle)
+	$: pagetitle = currentpath.slice(1).charAt(0).toUpperCase() + currentpath.slice(2);
 
+	$: {
+		if (pagetitle == '') {
+			pagetitle = 'Home';
+		}
+	}
+
+	$: isArticle = /News\/.+/.test(pagetitle);
 </script>
 
 <head>
-    {#if (!isArticle)}
-        <title>The Jambos Casa - {pagetitle}</title>
-    {/if}
-    
-    <meta content="/images/jambojudah.png" property="og:image" />
-    <meta property="og:image:width" content="1080">
-    <meta property="og:image:height" content="1080">
+	{#if !isArticle}
+		<title>The Jambos Casa - {pagetitle}</title>
+	{/if}
+
+	<meta content="/images/jambojudah.png" property="og:image" />
 </head>
 
 <Redirector />
-<Header token={data.token}/>
+<Header token={data.token} />
 
 <main>
-    <slot />
+	<slot />
 </main>
