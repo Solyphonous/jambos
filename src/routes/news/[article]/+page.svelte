a5b1249094e2b7619adbcd7d9f0ddf85b0daf217
Soly <82676469+Solyphonous@users.noreply.github.com>
2025-02-07 15:12:26 +0000

    make article scripts execute

--- src/routes/news/[article]/+page.svelte
+++ src/routes/news/[article]/+page.svelte
@@ -1,8 +1,23 @@
 <script>
+	import { onMount } from 'svelte';
+
 	export let data;
 
 	var date = new Date(Number(data.name));
 	var datestring = `${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}`;
+
+	onMount(() => {
+		const parser = new DOMParser()
+		const doc = parser.parseFromString(data.content, 'text/html')
+		const scripts = doc.querySelectorAll('script')
+
+		scripts.forEach(script => {
+      		const newScript = document.createElement('script');
+      		newScript.textContent = script.textContent;
+			console.log(newScript.textContent)
+      		document.body.appendChild(newScript).remove()
+    	});
+	}) 
 </script>
 
 <svelte:head>
