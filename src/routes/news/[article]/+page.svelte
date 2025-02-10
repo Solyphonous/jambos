725a7d5c89c255f2466e4e2ffb591f8e7b103fce
Solyphonous <82676469+Solyphonous@users.noreply.github.com>
2025-02-10 19:18:34 +0000

    add comment section on articles

--- src/routes/news/[article]/+page.svelte
+++ src/routes/news/[article]/+page.svelte
@@ -6,57 +6,87 @@
 	export let data;
 	export let form;
 
-	var date = new Date(Number(data.name));
+	const article = data.article;
+	const comments = data.comments;
+
+	var date = new Date(Number(article.name));
 	var datestring = `${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}`;
 
 	onMount(() => {
-		const parser = new DOMParser()
-		const doc = parser.parseFromString(data.content, 'text/html')
-		const scripts = doc.querySelectorAll('script')
+		const parser = new DOMParser();
+		const doc = parser.parseFromString(article.content, 'text/html');
+		const scripts = doc.querySelectorAll('script');
 
-		scripts.forEach(script => {
-      		const newScript = document.createElement('script');
-      		newScript.textContent = script.textContent;
-			console.log(newScript.textContent)
-      		document.body.appendChild(newScript).remove()
-    	});
-	}) 
+		scripts.forEach((script) => {
+			const newScript = document.createElement('script');
+			newScript.textContent = script.textContent;
+			console.log(newScript.textContent);
+			document.body.appendChild(newScript).remove();
+		});
+	});
 </script>
 
 <svelte:head>
-	<title>The Jambos Casa - {data.title}</title>
-	<meta content="Jambonian News: {data.title}" property="og:title" />
-	<meta content={data.thumbnail} property="og:image" />
+	<title>The Jambos Casa - {article.title}</title>
+	<meta content="Jambonian News: {article.title}" property="og:title" />
+	<meta content={article.thumbnail} property="og:image" />
 </svelte:head>
 
-<h1>{data.title}</h1>
-<h2>by {data.author}</h2>
+<h1>{article.title}</h1>
+<h2>by {article.author}</h2>
 <p><b>{datestring}</b></p>
-<div>{@html data.content}</div>
+<div>{@html article.content}</div>
 
 <div class="create-comment">
 	<form method="POST">
 		<label for="comment">
 			Comment
-			<hr>
+			<hr />
 			{#if data.token}
-				<textarea name="comment" id="comment" placeholder="Start writing your comment..." rows=5 required></textarea>
-				<input type="submit" value="Post">
+				<textarea
+					name="comment"
+					id="comment"
+					placeholder="Start writing your comment..."
+					rows="5"
+					required
+				></textarea>
+				<input type="submit" value="Post" />
 			{:else}
-				<p class="sign-in-please"><a href="/login?redirect=/news/{data.name}"><b>Sign in</b></a> to comment!</p>
+				<p class="wide">
+					<a href="/login?redirect=/news/{article.name}"><b>Sign in</b></a> to comment!
+				</p>
 			{/if}
 		</label>
 	</form>
 </div>
 
+{#each comments as comment}
+	<div class="comment">
+		<h2 class="wide">{comment.username}</h2>
+		<p class="wide">{comment.content}</p>
+		<h5 class="wide">{comment.createdAt}</h5>
+		<hr />
+	</div>
+{/each}
+
+<Warning toast={form?.toast} />
+
 <style>
+	.comment {
+		margin-left: auto;
+		margin-right: auto;
+		max-width: clamp(400px, 80%, 80%);
+		margin-top: 2rem;
+		margin-bottom: 2rem;
+	}
+
 	.create-comment {
 		box-sizing: border-box;
 		display: block;
 		margin-left: auto;
 		margin-right: auto;
 		max-width: clamp(400px, 80%, 80%);
-		border: .2rem dashed white;
+		border: 0.2rem dashed white;
 
 		margin-top: 2rem;
 		margin-bottom: 2rem;
@@ -67,10 +97,4 @@
 	hr {
 		border-top: 2px solid white;
 	}
-
-	p.sign-in-please {
-		max-width: 100%;
-	}
 </style>
-
-<Warning toast={form?.toast} />
