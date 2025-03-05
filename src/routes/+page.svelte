6c9cf9afdf22d383a6f3cce1a6a21f70c773135b
Seb <79903364+Plasma303@users.noreply.github.com>
2025-03-05 19:51:32 +0000

    homepageupdate

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,29 @@
-<h1>Hello jambonians</h1>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Color Changing Text</title>
+    <style>
+        @keyframes colorChange {
+            0% { color: red; }
+            25% { color: blue; }
+            50% { color: green; }
+            75% { color: yellow; }
+            100% { color: red; }
+        }
+
+        .bing {
+            font-weight: bold;
+            animation: colorChange 2s infinite;
+        }
+    </style>
+</head>
+
+<h1 class="bing">Hello jambonians</h1>
 <p>find my about page</p>
 
-<p style="text-align: center;"> Merry cash day from the Jambos team here in Earth </p>
+<p style="text-align: center;">Happy Spring to all Jambo Fans </p>
     
-<img src="/images/nathanmoney.jpg" alt="hi" style="margin:auto;display:block">
+<img src="/images/nathanfancy.jpg" alt="hi" style="margin:auto;display:block">
 <button><a href="/linganguli">press here for evil page</a></button>
 
 <style>
