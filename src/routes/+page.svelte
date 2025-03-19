037c9062de14977378c472b6fbc743fa6f833e71
Solyphonous <82676469+Solyphonous@users.noreply.github.com>
2025-03-19 21:11:22 +0000

    Fix sebastian schitzophrenic code

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,35 +1,43 @@
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Color Changing Text</title>
-    <style>
-        @keyframes colorChange {
-            0% { color: red; }
-            25% { color: blue; }
-            50% { color: green; }
-            75% { color: yellow; }
-            100% { color: red; }
-        }
-
-        .bing {
-            font-weight: bold;
-            animation: colorChange 2s infinite;
-        }
-    </style>
+	<meta charset="UTF-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 </head>
 
 <h1 class="bing">Hello jambonians</h1>
 <p>find my about page</p>
 
-<p style="text-align: center;">Happy Spring to all Jambo Fans </p>
-    
-<img src="/images/nathanfancy.jpg" alt="hi" style="margin:auto;display:block">
+<p style="text-align: center;">Happy Spring to all Jambo Fans</p>
+
+<img src="/images/nathanfancy.jpg" alt="hi" style="margin:auto;display:block" />
 <button><a href="/linganguli">press here for evil page</a></button>
 
 <style>
-    button {
-        background-color: red;
-        margin: auto;
-        display: block;
-    }
+	button {
+		background-color: red;
+		margin: auto;
+		display: block;
+	}
+
+	@keyframes colorChange {
+		0% {
+			color: red;
+		}
+		25% {
+			color: blue;
+		}
+		50% {
+			color: green;
+		}
+		75% {
+			color: yellow;
+		}
+		100% {
+			color: red;
+		}
+	}
+
+	.bing {
+		font-weight: bold;
+		animation: colorChange 2s infinite;
+	}
 </style>
