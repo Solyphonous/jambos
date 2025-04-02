944d0d8b7f56d661a2a985d60633cbf9d23d6024
Soly <82676469+Solyphonous@users.noreply.github.com>
2025-04-02 10:45:01 +0100

    prepare for the notificatiens if we ever add them

--- src/routes/news/+page.svelte
+++ src/routes/news/+page.svelte
@@ -1,7 +1,19 @@
 <script>
 	export let data;
 	import Article from '../../lib/news/article.svelte';
+
+	function click() {
+		alert("sorry this dont work yet but maybe eventually")
+	}
+
 </script>
+
+<h1>News</h1>
+<p><i>Certified most best jambonian news source of the millenium by Jambos INC.</i></p>
+<hr>
+<p>Love jambos? Pressing here for notifications EVERY time there is news article!!!</p>
+<button on:click={click} class="center-horizontally">yes please. I would love that.</button>
+<br>
 
 <div class="article-grid">
 	{#each data.articles as article}
