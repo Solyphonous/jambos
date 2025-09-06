ffd79092baa50ed737ede4a08000ebb66ac7148b
Solyphonous <82676469+Solyphonous@users.noreply.github.com>
2025-09-06 14:09:30 +0100

    Make gab link pop out

--- src/routes/memorial/+page.svelte
+++ src/routes/memorial/+page.svelte
@@ -1,1 +1,3 @@
-<a href="https://www.youtube.com/@CabbageChad"><img src="images/died.jpg" alt="died man" /></a>
+<a href="https://www.youtube.com/@CabbageChad" target="_blank"
+	><img src="images/died.jpg" alt="died man" /></a
+>
